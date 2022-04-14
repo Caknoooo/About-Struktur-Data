6c310b6760bc5c9b56b44536f7e5b67200a7b8d4
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-04-14 10:02:18 +0700

    Modul 1

---
+++ Praktikum Struktur Data/Kelas A/Modul 1/genap dulu baru ganjil.c
@@ -0,0 +1,135 @@
+/**
+ * Implementasi ADT PriorityQueue menggunakan Linked List
+ * [default: minimum priority]
+ * 
+ * Dibuat dan ditulis oleh Bayu Laksana
+ * -- tanggal 22 Januari 2019
+ * Struktur Data 2020
+ * Implementasi untuk bahasa C
+ * 
+ * !!NOTE!!
+ * cara menggunakan lihat pada fungsi main()
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/* Struktur Node */
+
+typedef struct pqueueNode_t {
+    int data;
+    struct pqueueNode_t *next;
+} PQueueNode;
+
+/* Struktur ADT PriorityQueue menggunakan Linked List */
+
+// Prioritas default: nilai minimum
+typedef struct pqueue_t {
+    PQueueNode *_top;
+    unsigned _size;
+} PriorityQueue;
+
+void pqueue_init(PriorityQueue *pqueue)
+{
+    pqueue->_top = NULL;
+    pqueue->_size = 0;
+}
+
+bool pqueue_isEmpty(PriorityQueue *pqueue) {
+    return (pqueue->_top == NULL);
+}
+
+void pqueue_push(PriorityQueue *pqueue, int value)
+{
+    PQueueNode *temp = pqueue->_top;
+    PQueueNode *newNode = \
+        (PQueueNode*) malloc (sizeof(PQueueNode));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (pqueue_isEmpty(pqueue)) {
+        pqueue->_top = newNode;
+        return;
+    }
+
+    if (value < pqueue->_top->data) {
+        newNode->next = pqueue->_top;
+        pqueue->_top = newNode;
+    }
+    else {
+        while ( temp->next != NULL && 
+                temp->next->data < value)
+            temp = temp->next;
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void pqueue_pushx(PriorityQueue *pqueue, int value)
+{
+    PQueueNode *temp = pqueue->_top;
+    PQueueNode *newNode = \
+        (PQueueNode*) malloc (sizeof(PQueueNode));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (pqueue_isEmpty(pqueue)) {
+        pqueue->_top = newNode;
+        return;
+    }
+
+    if (value > pqueue->_top->data) {
+        newNode->next = pqueue->_top;
+        pqueue->_top = newNode;
+    }
+    else {
+        while ( temp->next != NULL && 
+                temp->next->data > value)
+            temp = temp->next;
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void pqueue_pop(PriorityQueue *pqueue)
+{
+    if (!pqueue_isEmpty(pqueue)) {
+        PQueueNode *temp = pqueue->_top;
+        pqueue->_top = pqueue->_top->next;
+        free(temp);
+    }
+}
+
+int pqueue_top(PriorityQueue *pqueue) {
+    if (!pqueue_isEmpty(pqueue))
+        return pqueue->_top->data;
+    else return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    PriorityQueue ganjil, genap;
+    pqueue_init(&ganjil);
+    pqueue_init(&genap);
+    int t, x;
+    scanf("%d", &t);
+    while(t--)
+    {
+        scanf("%d", &x);
+        if(!(x % 2))
+            pqueue_push(&genap, x);
+        else
+            pqueue_pushx(&ganjil, x);
+    }
+    while(!pqueue_isEmpty(&genap))
+    {
+        printf("%d\n", pqueue_top(&genap));
+        pqueue_pop(&genap);
+    }
+    while(!pqueue_isEmpty(&ganjil))
+    {
+        printf("%d\n", pqueue_top(&ganjil));
+        pqueue_pop(&ganjil);
+    }
+}
