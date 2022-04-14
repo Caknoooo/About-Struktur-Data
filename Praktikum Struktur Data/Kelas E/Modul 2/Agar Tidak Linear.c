d8f0da42b70a8e7128e5957bca565a8369efd577
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-04-14 09:34:04 +0700

    Create Agar Tidak Linear.c

---
+++ Praktikum Struktur Data/Kelas E/Modul 2/Agar Tidak Linear.c
@@ -0,0 +1,198 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/**
+ * Node structure and
+ * uniqueBST structure
+ */
+
+typedef struct bstnode_t {
+    int key;
+    struct bstnode_t \
+        *left, *right;
+} BSTNode;
+
+typedef struct bst_t {
+    BSTNode *_root;
+    unsigned int _size;
+} BST;
+int count = 0;
+
+/**
+ * !!! WARNING UTILITY FUNCTION !!!
+ * Recognized by prefix "__bst__"
+ * ---------------------------------------------
+ * Note that you better never access these functions, 
+ * unless you need to modify or you know how these functions work.
+ */
+
+BSTNode* __bst__createNode(int value) {
+    BSTNode *newNode = (BSTNode*) malloc(sizeof(BSTNode));
+    newNode->key = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+BSTNode* __bst__insert(BSTNode *root, int value) {
+    if (root == NULL) 
+        return __bst__createNode(value);
+
+    if (value < root->key)
+        root->left = __bst__insert(root->left, value);
+    else if (value > root->key)
+        root->right = __bst__insert(root->right, value);
+    
+    return root;
+}
+
+BSTNode* __bst__search(BSTNode *root, int value) {
+    while (root != NULL) {
+        if (value < root->key)
+            root = root->left;
+        else if (value > root->key)
+            root = root->right;
+        else
+            return root;
+    }
+    return root;
+}
+
+BSTNode* __bst__findMinNode(BSTNode *node) {
+    BSTNode *currNode = node;
+    while (currNode && currNode->left != NULL)
+        currNode = currNode->left;
+    
+    return currNode;
+}
+
+BSTNode* __bst__remove(BSTNode *root, int value) {
+    if (root == NULL) return NULL;
+
+    if (value > root->key) 
+        root->right = __bst__remove(root->right, value);
+    else if (value < root->key) 
+        root->left = __bst__remove(root->left, value);
+    else {
+        
+        if (root->left == NULL) {
+            BSTNode *rightChild = root->right;
+            free(root);
+            return rightChild;
+        }
+        else if (root->right == NULL) {
+            BSTNode *leftChild = root->left;
+            free(root);
+            return leftChild;
+        }
+
+        BSTNode *temp = __bst__findMinNode(root->right);
+        root->key     = temp->key;
+        root->right   = __bst__remove(root->right, temp->key);
+    }
+    return root;
+}
+
+void __bst__inorder(BSTNode *root) {
+    if (root) {
+        __bst__inorder(root->left);
+        printf("%d ", root->key);
+        __bst__inorder(root->right);
+    }
+}
+
+void __bst__postorder(BSTNode *root) {
+    if (root) {
+        __bst__postorder(root->left);
+        __bst__postorder(root->right);
+        printf("%d ", root->key);
+    }
+}
+
+void __bst__preorder(BSTNode *root) {
+    if (root) {
+        printf("%d ", root->key);
+        __bst__preorder(root->left);
+        __bst__preorder(root->right);
+    }
+}
+
+/**
+ * PRIMARY FUNCTION
+ * ---------------------------
+ * Accessible and safe to use.
+ */
+
+void bst_init(BST *bst) {
+    bst->_root = NULL;
+    bst->_size = 0u;
+}
+
+bool bst_isEmpty(BST *bst) {
+    return bst->_root == NULL;
+}
+
+bool bst_find(BST *bst, int value) {
+    BSTNode *temp = __bst__search(bst->_root, value);
+    if (temp == NULL)
+        return false;
+    
+    if (temp->key == value)
+        return true;
+    else
+        return false;
+}
+
+void bst_insert(BST *bst, int value) {
+    if (!bst_find(bst, value)) {
+        bst->_root = __bst__insert(bst->_root, value);
+        bst->_size++;
+    }
+}
+
+void bst_remove(BST *bst, int value) {
+    if (bst_find(bst, value)) {
+        bst->_root = __bst__remove(bst->_root, value);
+        bst->_size--;
+    }
+}
+
+void binarySearch(BST *bst,int arr[100000], int l, int r)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+        bst_insert(bst,arr[mid]);
+        if(r>=l){
+          binarySearch(bst, arr, l, mid - 1);
+          binarySearch(bst, arr, mid + 1, r);
+        }
+    }
+}
+
+int main()
+{
+    BST set;
+    bst_init(&set);
+    int t;
+    scanf("%d", &t);
+    int arr[t + 1];
+    for(int i = 0; i < t; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < t; i++)
+    {
+        for(int j = i + 1; j < t; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    binarySearch(&set, arr, 0, t - 1);
+    __bst__preorder(set._root);
+    puts("");
+}
