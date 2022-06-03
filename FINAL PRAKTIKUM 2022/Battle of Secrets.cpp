63cfb77cac6f0d316a7631be357cff4be626b598
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-06-03 19:55:47 +0700

    FP-2022

---
+++ FINAL PRAKTIKUM 2022/Battle of Secrets.cpp
@@ -0,0 +1,260 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#ifdef WIN32
+inline int getchar_unlocked() 
+    {return _getchar_nolock(); }
+#endif
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define AVL tree<int, null_type,less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define psi pair<string, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(),x.end()
+#define revAll(x) x.rbegin(),x.rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define loop(t) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define seev(v,n) for(int i = 0; i < n; i++){int x; cin >> x; v.push_back(x);}
+#define sees(s,n) for(int i = 0; i < n; i++){int x; cin >> x; s.insert(x);}
+#define MAX 10005
+#define MOD 1000000007
+typedef long long ll;
+
+typedef struct bstnode_t {
+    int key;
+    struct bstnode_t \
+        *left, *right;
+} BSTNode;
+
+typedef struct bst_t {
+    BSTNode *_root;
+    unsigned int _size;
+} BST;
+
+/**
+ * !!! WARNING UTILITY FUNCTION !!!
+ * Recognized by prefix "__bst__"
+ * ---------------------------------------------
+ * Note that you better never access these functions, 
+ * unless you need to modify or you know how these functions work.
+ */
+
+BSTNode* __bst__createNode(int value) {
+    BSTNode *newNode = (BSTNode*) malloc(sizeof(BSTNode));
+    newNode->key = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+BSTNode* __bst__insert(BSTNode *root, int value) {
+    if (root == NULL) 
+        return __bst__createNode(value);
+
+    if (value < root->key)
+        root->left = __bst__insert(root->left, value);
+    else if (value > root->key)
+        root->right = __bst__insert(root->right, value);
+    
+    return root;
+}
+
+BSTNode* __bst__search(BSTNode *root, int value) {
+    while (root != NULL) {
+        if (value < root->key)
+            root = root->left;
+        else if (value > root->key)
+            root = root->right;
+        else
+            return root;
+    }
+    return root;
+}
+
+BSTNode* __bst__findMinNode(BSTNode *node) {
+    BSTNode *currNode = node;
+    while (currNode && currNode->left != NULL)
+        currNode = currNode->left;
+    
+    return currNode;
+}
+
+BSTNode* __bst__remove(BSTNode *root, int value) {
+    if (root == NULL) return NULL;
+
+    if (value > root->key) 
+        root->right = __bst__remove(root->right, value);
+    else if (value < root->key) 
+        root->left = __bst__remove(root->left, value);
+    else {
+        
+        if (root->left == NULL) {
+            BSTNode *rightChild = root->right;
+            free(root);
+            return rightChild;
+        }
+        else if (root->right == NULL) {
+            BSTNode *leftChild = root->left;
+            free(root);
+            return leftChild;
+        }
+
+        BSTNode *temp = __bst__findMinNode(root->right);
+        root->key     = temp->key;
+        root->right   = __bst__remove(root->right, temp->key);
+    }
+    return root;
+}
+
+void __bst__inorder(BSTNode *root) {
+    if (root) {
+        __bst__inorder(root->left);
+        printf("%d ", root->key);
+        __bst__inorder(root->right);
+    }
+}
+
+void __bst__postorder(BSTNode *root) {
+    if (root) {
+        __bst__postorder(root->left);
+        __bst__postorder(root->right);
+        printf("%d ", root->key);
+    }
+}
+
+void __bst__preorder(BSTNode *root) {
+    if (root) {
+        printf("%d ", root->key);
+        __bst__preorder(root->left);
+        __bst__preorder(root->right);
+    }
+}
+
+void bst_init(BST *bst) {
+    bst->_root = NULL;
+    bst->_size = 0u;
+}
+
+bool bst_isEmpty(BST *bst) {
+    return bst->_root == NULL;
+}
+
+bool bst_find(BST *bst, int value) {
+    BSTNode *temp = __bst__search(bst->_root, value);
+    if (temp == NULL)
+        return false;
+    
+    if (temp->key == value)
+        return true;
+    else
+        return false;
+}
+
+void bst_insert(BST *bst, int value) {
+    if (!bst_find(bst, value)) {
+        bst->_root = __bst__insert(bst->_root, value);
+        bst->_size++;
+    }
+}
+
+void bst_remove(BST *bst, int value) {
+    if (bst_find(bst, value)) {
+        bst->_root = __bst__remove(bst->_root, value);
+        bst->_size--;
+    }
+}
+
+void bst_inorder(BST *bst) {
+    __bst__inorder(bst->_root);
+}
+
+void bst_postorder(BST *bst) {
+    __bst__postorder(bst->_root);
+}
+
+void bst_preorder(BST *bst) {
+    __bst__preorder(bst->_root);
+}
+
+void gasPreorder(BSTNode *root, set<int> &node)
+{
+    if(root)
+    {
+        gasPreorder(root->left, node);
+        gasPreorder(root->right, node);
+        if(root->left == NULL && root->right == NULL && node.find(root->key) != node.end())
+            node.erase(root->key);
+    }
+}
+
+void bst_removee(BSTNode *root, set<int> &node)
+{
+    if(root == NULL)
+        return;
+        
+    BSTNode *temp = root;
+    
+    if(!temp)
+        return;
+        
+    queue<BSTNode*> q;
+    q.push(temp);
+    
+    while(!q.empty())
+    {
+        int sz = q.size();
+        for(int i = 1; i <= sz; i++)
+        {
+            BSTNode *temp2 = q.front();
+            q.pop();
+            if(i == 1 || i == sz)
+                node.erase(temp2->key);
+            if(temp2->left != NULL)
+                q.push(temp2->left);
+            if(temp2->right != NULL)
+                q.push(temp2->right);
+        }
+    }
+    gasPreorder(root, node);
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    BST bst;
+    bst_init(&bst);
+    set<int> node;
+    int n, in;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> in;
+        bst_insert(&bst, in);
+        node.insert(in);
+    } 
+    bst_removee(bst._root, node);
+    
+    if(node.size() == 0)
+        cout << "It's not safe\n";
+    else{
+        set<int>::iterator it;
+        for(it = node.begin(); it != node.end(); it++)
+        {
+            cout << *it << " ";
+        }
+        cout << '\n';
+    }
+}
