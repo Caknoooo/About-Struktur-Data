2330b5944b237c6ddd882dda051dc4f2cea52f1b
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2022-06-03 20:06:53 +0700

    FP-2022

---
+++ FINAL PRAKTIKUM 2022/Rindu.cpp
@@ -0,0 +1,313 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#ifdef WIN32
+inline int getchar_unlocked() 
+    {return _getchar_nolock(); }
+#endif
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define vi vector<int>
+#define vb vector<bool>
+#define vc vector<char>
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define psi pair<string, int>
+#define pll pair<long long, long long>
+#define all(x) x.begin(),x.end()
+#define revAll(x) x.rbegin(),x.rend()
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define loop(t) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i < b; i++)
+//#define AVL tree<int, null_type,less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define seev(v,n) for(int i = 0; i < n; i++){int x; cin >> x; v.push_back(x);}
+#define sees(s,n) for(int i = 0; i < n; i++){int x; cin >> x; s.insert(x);}
+#define MAX 10005
+#define MOD 1000000007
+typedef long long ll;
+
+typedef struct AVLNode_t
+{
+    int data;
+    struct AVLNode_t *left,*right;
+    int height;
+}AVLNode;
+
+typedef struct AVL_t
+{
+    AVLNode *_root;
+    unsigned int _size;
+}AVL;
+
+AVLNode* _avl_createNode(int value) {
+    AVLNode *newNode = (AVLNode*) malloc(sizeof(AVLNode));
+    newNode->data = value;
+    newNode->height=1;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+AVLNode* _search(AVLNode *root, int value) {
+    while (root != NULL) {
+        if (value < root->data)
+            root = root->left;
+        else if (value > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return root;
+}
+
+int _getHeight(AVLNode* node){
+    if(node==NULL)
+        return 0; 
+    return node->height;
+}
+
+int _max(int a,int b){
+    return (a > b)? a : b;
+}
+
+AVLNode* _rightRotate(AVLNode* pivotNode) {
+
+    AVLNode* newParrent=pivotNode->left;
+    pivotNode->left=newParrent->right;
+    newParrent->right=pivotNode;
+
+    pivotNode->height=_max(_getHeight(pivotNode->left),
+                      _getHeight(pivotNode->right))+1;
+    newParrent->height=_max(_getHeight(newParrent->left),
+                       _getHeight(newParrent->right))+1;
+    
+    return newParrent;
+}
+
+AVLNode* _leftRotate(AVLNode* pivotNode) {
+
+    AVLNode* newParrent=pivotNode->right;
+    pivotNode->right=newParrent->left;
+    newParrent->left=pivotNode;
+
+    pivotNode->height=_max(_getHeight(pivotNode->left),
+                      _getHeight(pivotNode->right))+1;
+    newParrent->height=_max(_getHeight(newParrent->left),
+                       _getHeight(newParrent->right))+1;
+    
+    return newParrent;
+}
+
+AVLNode* _leftCaseRotate(AVLNode* node){
+    return _rightRotate(node);
+}
+
+AVLNode* _rightCaseRotate(AVLNode* node){
+    return _leftRotate(node);
+}
+
+AVLNode* _leftRightCaseRotate(AVLNode* node){
+    node->left=_leftRotate(node->left);
+    return _rightRotate(node);
+}
+
+AVLNode* _rightLeftCaseRotate(AVLNode* node){
+    node->right=_rightRotate(node->right);
+    return _leftRotate(node);
+}
+
+int _getBalanceFactor(AVLNode* node){
+    if(node==NULL)
+        return 0;
+    return _getHeight(node->left)-_getHeight(node->right);
+}
+
+AVLNode* _insert_AVL(AVL *avl,AVLNode* node,int value) {
+    
+    if(node==NULL) // udah mencapai leaf
+        return _avl_createNode(value);
+    if(value < node->data)
+        node->left = _insert_AVL(avl,node->left,value);
+    else if(value > node->data)
+        node->right = _insert_AVL(avl,node->right,value);
+    
+    node->height= 1 + _max(_getHeight(node->left),_getHeight(node->right)); 
+
+    int balanceFactor=_getBalanceFactor(node);
+    
+    if(balanceFactor > 1 && value < node->left->data) // skewed kiri (left-left case)
+        return _leftCaseRotate(node);
+    if(balanceFactor > 1 && value > node->left->data) // 
+        return _leftRightCaseRotate(node);
+    if(balanceFactor < -1 && value > node->right->data)
+        return _rightCaseRotate(node);
+    if(balanceFactor < -1 && value < node->right->data)
+        return _rightLeftCaseRotate(node);
+    
+    return node;
+}
+
+AVLNode* _findMinNode(AVLNode *node) {
+    AVLNode *currNode = node;
+    while (currNode && currNode->left != NULL)
+        currNode = currNode->left;
+    return currNode;
+}
+
+AVLNode* _remove_AVL(AVLNode* node,int value){
+    if(node==NULL)
+        return node;
+    if(value > node->data)
+        node->right=_remove_AVL(node->right,value);
+    else if(value < node->data)
+        node->left=_remove_AVL(node->left,value);
+    else{
+        AVLNode *temp;
+        if((node->left==NULL)||(node->right==NULL)){
+            temp=NULL;
+            if(node->left==NULL) temp=node->right;  
+            else if(node->right==NULL) temp=node->left;
+            
+            if(temp==NULL){
+                temp=node;
+                node=NULL;
+            }
+            else
+                *node=*temp;   
+            
+            free(temp);
+        }
+        else{
+            temp = _findMinNode(node->right);
+            node->data=temp->data;
+            node->right=_remove_AVL(node->right,temp->data);
+        }    
+    }
+
+    if(node==NULL) return node;
+    
+    node->height=_max(_getHeight(node->left),_getHeight(node->right))+1;
+
+    int balanceFactor= _getBalanceFactor(node);
+    
+    if(balanceFactor>1 && _getBalanceFactor(node->left)>=0) 
+        return _leftCaseRotate(node);
+
+    if(balanceFactor>1 && _getBalanceFactor(node->left)<0) 
+        return _leftRightCaseRotate(node);
+  
+    if(balanceFactor < -1 && _getBalanceFactor(node->right)<=0) 
+        return _rightCaseRotate(node);
+
+    if(balanceFactor < -1 && _getBalanceFactor(node->right)>0) 
+        return _rightLeftCaseRotate(node);
+    
+    return node;
+}
+
+void avl_init(AVL *avl) {
+    avl->_root = NULL;
+    avl->_size = 0u;
+}
+
+bool avl_isEmpty(AVL *avl) {
+    return avl->_root == NULL;
+}
+
+bool avl_find(AVL *avl, int value) {
+    AVLNode *temp = _search(avl->_root, value);
+    if (temp == NULL)
+        return false;
+    
+    if (temp->data == value)
+        return true;
+    else
+        return false;
+}
+
+void avl_insert(AVL *avl,int value){
+    if(!avl_find(avl,value)){
+        avl->_root=_insert_AVL(avl,avl->_root,value);
+        avl->_size++;
+    }
+
+}
+
+void avl_remove(AVL *avl,int value){
+    if(avl_find(avl,value)){
+        avl->_root=_remove_AVL(avl->_root,value);
+        avl->_size--;
+    }
+
+}
+
+void preorder(AVLNode *root) {
+    if (root) {
+        preorder(root->left);
+        printf("%d ", root->data);
+        preorder(root->right);
+    }
+}
+
+AVLNode *lca(AVLNode* root, int n1, int n2)
+{
+    while (root != NULL)
+    {
+
+        if (root->data > n1 && root->data > n2)
+            root = root->left;
+ 
+        else if (root->data < n1 && root->data < n2)
+            root = root->right;
+ 
+        else 
+            break;
+    }
+    return root;
+}
+
+int distance(AVLNode *root, int x)
+{
+    int jrk = 0;
+    while(root != NULL)
+    {
+        if(x < root->data)
+        {
+            jrk += abs(root->left->data - root->data);
+            root = root->left;
+        }
+        else if(x > root->data)
+        {
+            jrk += abs(root->right->data - root->data);
+            root = root->right;
+        }
+        else
+            return jrk;
+    }
+    return jrk;
+}
+
+int main(){
+    AVL avlku;
+    avl_init(&avlku);
+    int n, jrk1, jrk2, dist = 0;
+    cin >> n;
+    while(n--)
+    {
+        int in;
+        cin >> in;
+        avl_insert(&avlku, in);
+    }
+    
+    cin >> jrk1 >> jrk2;
+    dist += distance(lca(avlku._root, jrk1, jrk2), jrk1);
+    dist += distance(lca(avlku._root, jrk1, jrk2), jrk2);
+    
+    cout << dist << "\n";
+    return 0;
+
+}
